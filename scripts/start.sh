b8c1120243339f57404236c48b58a2948feb1c70
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-06-09 19:49:03 +0200

    Init

---
+++ scripts/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export ASPNETCORE_ENVIRONMENT=local
+cd src/Pacco.Services.Availability
+dotnet run
---
+++ src/Pacco.Services.Availability/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Convey;
+using Convey.CQRS.Commands;
+using Convey.CQRS.Events;
+using Convey.CQRS.Queries;
+using Convey.WebApi;
+using Convey.WebApi.CQRS;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace Pacco.Services.Availability
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+            => await WebHost.CreateDefaultBuilder(args)
+                .ConfigureServices(services => services
+                    .AddConvey()
+                    .AddCommandHandlers()
+                    .AddEventHandlers()
+                    .AddQueryHandlers()
+                    .AddWebApi())
+                .Configure(app => app
+                    .UseErrorHandler()
+                    .UsePublicMessages()
+                    .UseEndpoints(endpoints => endpoints
+                        .Get("", ctx => ctx.Response.WriteAsync("Welcome to Pacco Availability Service!"))))
+                .Build()
+                .RunAsync();
+    }
+}
