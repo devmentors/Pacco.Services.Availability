4a4f4c8d3c85616c83786150fa65f87366619b1d
GooRiOn <foreverframenet@gmail.com>
2019-08-29 14:35:40 +0200

    Switched to Convey logging

--- src/Pacco.Services.Availability.Infrastructure/Logging/HandlerLogMessage.cs
+++
@@ -1,8 +0,0 @@
-namespace Pacco.Services.Availability.Infrastructure.Logging
-{
-    internal sealed class HandlerLogMessage
-    {
-        public string Before { get; set; }
-        public string After { get; set; }
-    }
-}
