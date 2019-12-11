0601c92b293543fbb70b345be882d17063cfaf92
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-12-11 23:39:21 +0100

    Fixed test

--- tests/Pacco.Services.Availability.Tests.Integration/Async/AddResourceTests.cs
+++ tests/Pacco.Services.Availability.Tests.Integration/Async/AddResourceTests.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.TestHost;
-using Newtonsoft.Json;
 using Pacco.Services.Availability.Api;
 using Pacco.Services.Availability.Application.Commands;
 using Pacco.Services.Availability.Application.Events;
@@ -40,21 +36,9 @@ namespace Pacco.Services.Availability.Tests.Integration.Async
         
         private readonly MongoDbFixture<ResourceDocument, Guid> _mongoDbFixture;
         private readonly RabbitMqFixture _rabbitMqFixture;
-        private readonly HttpClient _httpClient;
-        
         private readonly Guid _resourceId;
         private readonly string[] _tags;
         private const string Exchange = "availability";
-
-        private HttpContent GetHttpContent(AddResource command)
-        {
-            var json = JsonConvert.SerializeObject(command);
-            var buffer = System.Text.Encoding.UTF8.GetBytes(json);
-            var byteContent = new ByteArrayContent(buffer);
-            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-
-            return byteContent;
-        }
 
         public AddResourceTests(PaccoApplicationFactory<Program> factory)
         {
@@ -62,9 +46,7 @@ namespace Pacco.Services.Availability.Tests.Integration.Async
             _tags = new[]{"tags"};
             _rabbitMqFixture = new RabbitMqFixture("availability");
             _mongoDbFixture = new MongoDbFixture<ResourceDocument, Guid>("Resources");
-
             factory.Server.AllowSynchronousIO = true;
-            _httpClient = factory.CreateClient();
         }
         
         public void Dispose()
