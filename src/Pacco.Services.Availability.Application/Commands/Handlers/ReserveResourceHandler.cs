a8fa7593074a0e6f5ffb2463eddde72d9c9b3a1f
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2020-04-15 17:06:42 +0200

    Reserve resource

--- src/Pacco.Services.Availability.Application/Commands/Handlers/ReserveResourceHandler.cs
+++ src/Pacco.Services.Availability.Application/Commands/Handlers/ReserveResourceHandler.cs
@@ -1,13 +1,31 @@
 using System.Threading.Tasks;
 using Convey.CQRS.Commands;
+using Pacco.Services.Availability.Application.Exceptions;
+using Pacco.Services.Availability.Core.Repositories;
+using Pacco.Services.Availability.Core.ValueObjects;
 
 namespace Pacco.Services.Availability.Application.Commands.Handlers
 {
     public class ReserveResourceHandler : ICommandHandler<ReserveResource>
     {
+        private readonly IResourcesRepository _resourcesRepository;
+
+        public ReserveResourceHandler(IResourcesRepository resourcesRepository)
+        {
+            _resourcesRepository = resourcesRepository;
+        }
+        
         public async Task HandleAsync(ReserveResource command)
         {
-            await Task.CompletedTask;
+            var resource = await _resourcesRepository.GetAsync(command.ResourceId);
+            if (resource is null)
+            {
+                throw new ResourceNotFoundException(command.ResourceId);
+            }
+            
+            var reservation = new Reservation(command.DateTime, command.Priority);
+            resource.AddReservation(reservation);
+            await _resourcesRepository.UpdateAsync(resource);
         }
     }
 }
