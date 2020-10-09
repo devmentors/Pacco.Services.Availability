0d058b313c8f607ee3753f505acdcc74acf49141
GooRiOn <foreverframenet@gmail.com>
2020-10-09 12:02:53 +0200

    Added fabio integration

--- src/Pacco.Services.Availability.Infrastructure/Extensions.cs
+++ src/Pacco.Services.Availability.Infrastructure/Extensions.cs
@@ -8,6 +8,7 @@ using Convey.CQRS.Events;
 using Convey.CQRS.Queries;
 using Convey.Discovery.Consul;
 using Convey.HTTP;
+using Convey.LoadBalancing.Fabio;
 using Convey.MessageBrokers;
 using Convey.MessageBrokers.CQRS;
 using Convey.MessageBrokers.Outbox;
@@ -59,6 +60,7 @@ namespace Pacco.Services.Availability.Infrastructure
                 .AddMongo()
                 .AddHttpClient()
                 .AddConsul()
+                .AddFabio()
                 .AddRabbitMq()
                 .AddMessageOutbox(o => o.AddMongo())
                 .AddRedis()
